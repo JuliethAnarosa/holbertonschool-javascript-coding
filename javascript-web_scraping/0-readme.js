78c7c737a0f53792b4fd3f2f7e49bd225f3249b0
JuliethAnarosa <america02anarosa@gmail.com>
2023-09-24 19:23:00 -0500

    task

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+if (process.argv.length !== 3) {
+  console.error('Uso: node 0-readme.js <ruta del archivo>');
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (error, data) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(data);
+  }
+});
