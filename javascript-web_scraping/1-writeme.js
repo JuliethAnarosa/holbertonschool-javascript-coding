ee159be2d6b1a7d7e4e9cf3d382092b099eef808
JuliethAnarosa <america02anarosa@gmail.com>
2023-09-24 19:36:59 -0500

    task

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,19 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+const FileName = process.argv[2];
+const FileContent = process.argv[3];
 
-if (process.argv.length !== 4) {
-  console.error('Usage: ./1-writeme.js <file path> <string to write>');
-  process.exit(1);
-}
-
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-fs.writeFile(filePath, content, 'utf-8', (error) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(`Content written to ${filePath}`);
+fs.writeFile(FileName, FileContent, function (err) {
+  if (err) {
+    console.log(err);
   }
 });
