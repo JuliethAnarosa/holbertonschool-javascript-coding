b6632c0d0b2266f7e0c52102fb011bd1b4bf70dc
JuliethAnarosa <america02anarosa@gmail.com>
2023-10-13 11:53:47 -0500

    task

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,17 +1,17 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
 console.log('Welcome to Holberton School, what is your name?');
 
 rl.question('', (name) => {
-    console.log(`Your name is: ${name}`);
-    console.log('This important software is now closing');
-    rl.close();
+  console.log(`Your name is: ${name}`);
+  console.log('This important software is now closing');
+  rl.close();
 });
-rl.on('close',()=>{
-    process.exit(0)
+rl.on('close', () => {
+  process.exit(0);
 });
